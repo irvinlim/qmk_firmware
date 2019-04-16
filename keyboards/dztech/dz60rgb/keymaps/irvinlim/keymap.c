9ae3fdef06a510fa5bfbf7aebbb0f1d0a68cac98
Irvin Lim <irvinlimweiquan@gmail.com>
2019-04-16 02:44:47 -0400

    Add RGB helpers for layer state and caps lock state

--- keyboards/dztech/dz60rgb/keymaps/irvinlim/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/irvinlim/keymap.c
@@ -44,106 +44,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
 
-void rgb_matrix_layer_helper(uint8_t red, uint8_t green, uint8_t blue, bool default_layer) {
-  rgb_led led;
-  for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-    led = g_rgb_leds[i];
-    if (led.matrix_co.raw < 0xFF) {
-      if (led.modifier) {
-        rgb_matrix_set_color(i, red, green, blue);
-      }
-    }
+int numrow_index(int number) {
+  if (number < 0 || number > 12) {
+    return 0;
   }
+
+  return 13 - number;
 }
 
 void rgb_matrix_indicators_user(void) {
-  // uint8_t this_led = host_keyboard_leds();
-  // if (!g_suspend_state) {
-  //   switch (biton32(layer_state)) {
-  //     case _LAYER1:
-  //       rgb_matrix_layer_helper(0xFF, 0x00, 0x00, false);
-  //       break;
-  //     case _LAYER2:
-  //       rgb_matrix_layer_helper(0x00, 0xFF, 0x00, false);
-  //       break;
-  //     case _LAYER3:
-  //       if (this_led & (1 << USB_LED_NUM_LOCK)) {
-  //         rgb_matrix_set_color(13, 0xFF, 0x00, 0x00);
-  //       } else {
-  //         rgb_matrix_set_color(13, 0x00, 0x00, 0x00);
-  //       }
-  //       rgb_matrix_set_color(0, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(1, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(1, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(2, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(3, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(4, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(5, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(6, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(7, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(8, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(9, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(10, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(11, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(12, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(14, 0x00, 0x00, 0xFF);
-  //       rgb_matrix_set_color(15, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(16, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(17, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(18, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(19, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(20, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(21, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(22, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(23, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(24, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(25, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(26, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(27, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(28, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(29, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(30, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(31, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(32, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(33, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(34, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(35, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(36, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(37, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(38, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(39, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(40, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(41, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(42, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(43, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(44, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(45, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(46, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(47, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(48, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(49, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(50, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(51, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(52, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(53, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(54, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(55, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(56, 0x00, 0x00, 0x00);
-  //       rgb_matrix_set_color(57, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(58, 0x00, 0xFF, 0x00);
-  //       rgb_matrix_set_color(59, 0xFF, 0x00, 0x00);
-  //       rgb_matrix_set_color(60, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(61, 0xFF, 0xFF, 0x00);
-  //       rgb_matrix_set_color(62, 0x00, 0x00, 0x00);
-  //       break;
-  //     case _LAYER4:
-  //       rgb_matrix_layer_helper(0xFF, 0xFF, 0x00, false);
-  //       break;
-  //   }
-  // }
-  // if (this_led & (1 << USB_LED_CAPS_LOCK)) {
-  //   rgb_matrix_set_color(40, 0xFF, 0xFF, 0xFF);
-  // }
+  uint8_t this_led = host_keyboard_leds();
+
+  // Display current layer in Num row (if not default).
+  if (!g_suspend_state) {
+    rgb_matrix_set_color(numrow_index(biton32(layer_state)), 0x20, 0x20, 0x20);
+  }
+
+  // Enable Caps Lock lighting.
+  if (this_led & (1 << USB_LED_CAPS_LOCK)) {
+    rgb_matrix_set_color(40, 0xFF, 0xFF, 0xFF);
+  }
 }
 
 void matrix_init_user(void) {
