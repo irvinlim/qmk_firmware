d8045eecec8beb41e03b8f292ad9c7bfc0ec34c5
Irvin Lim <irvinlimweiquan@gmail.com>
2019-04-17 17:45:09 -0400

    Remove RSFT_T from right shift and only use it as KC_SLSH

--- keyboards/dztech/dz60rgb/keymaps/irvinlim/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/irvinlim/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // R3
         CMD_T(KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         // R4
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH), KC_UP, LT(MAC_FN_LAYER, KC_GRV),
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP, LT(MAC_FN_LAYER, KC_GRV),
         // R5
         KC_LCTL, KC_LALT, KC_LCMD, KC_SPC, KC_RCMD, MO(FN_LAYER), KC_LEFT, KC_DOWN, KC_RIGHT),
 
